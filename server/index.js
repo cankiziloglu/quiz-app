6b4eb935b53eb50f82b815804bc88049982db081
Can Gokhan Kiziloglu <117171733+cankiziloglu@users.noreply.github.com>
2024-01-04 15:18:41 +0300

    installed helmet & morgan

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 require('dotenv').config();
+const helmet = require('helmet')
+const morgan = require('morgan')
 
 const port = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
