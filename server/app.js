82c558fef10e5f919def0cf330015be4a0fafe88
Can Gokhan Kiziloglu <117171733+cankiziloglu@users.noreply.github.com>
2024-01-06 23:16:10 +0300

    all routes created

--- server/app.js
+++ server/app.js
@@ -2,28 +2,24 @@ const express = require('express');
 require('dotenv').config();
 const helmet = require('helmet');
 const morgan = require('morgan');
-const mongoose = require('mongoose');
-const quiz = require('./routes/quiz.route');
+const quiz = require('./routes/quizzes.route');
 const users = require('./routes/users.route');
-const results = require('./routes/results.route');
+const results = require('./routes/attempts.route');
+const questions = require('./routes/questions.route');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
-app.use('/api/quiz', quiz);
-// app.use('/api/user', users);
-// app.use('/api/result', results);
+app.use('/api/quizzes', quizzes);
+app.use('/api/users', users);
+app.use('/api/attempts', attempts);
+app.use('/api/questions', questions);
 
 if (app.get('env') === 'development') {
   app.use(morgan('tiny'));
   console.log('Morgan enabled...');
 }
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch((err) => console.error('Could not connect to MongoDB...', err));
 
 module.exports = app;
