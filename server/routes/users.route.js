d461bab3265cb77cb290f210e39a4ff53de3dd0f
Can Gokhan Kiziloglu <117171733+cankiziloglu@users.noreply.github.com>
2024-01-06 23:16:24 +0300

    Users route outlined

---
+++ server/routes/users.route.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const router = express.Router();
+
+// /api/users
+// Get all users (admin only)
+router.get('/', async (req, res) => {     // TODO: Add authentication and admin middleware here
+    
+})
+
+// Register a new user
+router.post('/', async (req, res) => {
+    
+})
+
+// /api/users/me
+// Get the profile of the logged-in user
+router.get('/me', async (req, res) => {   // TODO: Add authentication middleware here
+    
+})
+
+// Update the profile of the logged-in user
+router.put('/me', async (req, res) => {   // TODO: Add authentication middleware here
+    
+})
+
+// Get user profile by ID (admin only)
+router.get('/:user_id', async (req, res) => {  // TODO: Add authentication and admin middleware here
+    
+})
+
+// Delete user by ID (admin only)
+router.delete('/:user_id', async (req, res) => {  // TODO: Add authentication and admin middleware here
+    
+})
+
+
+module.exports = router;
