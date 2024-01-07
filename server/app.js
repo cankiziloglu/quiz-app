165f2a35a80ab94b95e685e1830a505bdf718ca1
Can Gokhan Kiziloglu <117171733+cankiziloglu@users.noreply.github.com>
2024-01-07 16:56:56 +0300

    Add Winston logger and error handling middleware

--- server/app.js
+++ server/app.js
@@ -2,10 +2,13 @@ const express = require('express');
 require('dotenv').config();
 const helmet = require('helmet');
 const morgan = require('morgan');
+require('express-async-errors');
+const logger = require('./middleware/logger');
 const quiz = require('./routes/quizzes.route');
 const users = require('./routes/users.route');
 const results = require('./routes/attempts.route');
 const questions = require('./routes/questions.route');
+
 
 const app = express();
 
@@ -16,6 +19,7 @@ app.use('/api/quizzes', quizzes);
 app.use('/api/users', users);
 app.use('/api/attempts', attempts);
 app.use('/api/questions', questions);
+app.use(logger);
 
 if (app.get('env') === 'development') {
   app.use(morgan('tiny'));
