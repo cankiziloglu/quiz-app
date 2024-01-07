aaf0c09579546aa0ac2d6013271df930d9484daa
Can Gokhan Kiziloglu <117171733+cankiziloglu@users.noreply.github.com>
2024-01-07 22:01:14 +0300

    questions endpoints done

---
+++ server/models/questions.model.js
@@ -0,0 +1,26 @@
+const Joi = require('joi');
+
+function validateQuestion(question) {
+  const schema = Joi.object({
+    content: Joi.string().min(3).max(1000).required(),
+    answers: Joi.array()
+      .items(
+        Joi.object({
+          content: Joi.string().min(1).max(500).required(),
+          is_correct: Joi.boolean().required(),
+        })
+      )
+      .length(4)
+      .required()
+      .custom((value, helper) => {
+        const correctAnswers = value.filter((answer) => answer.is_correct);
+        if (correctAnswers.length !== 1) {
+          return helper.message('There must be exactly 1 correct answer');
+        }
+        return value;
+      }, 'Answers Validation'),
+  });
+  return schema.validate(question);
+}
+
+exports.validate = validateQuestion;
--- server/routes/questions.route.js
+++ server/routes/questions.route.js
@@ -1,7 +1,128 @@
 const express = require('express');
+const _ = require('lodash');
+const prisma = require('../prisma/Client');
+const { validate } = require('../models/questions.model');
+const auth = require('../middleware/auth');
+const admin = require('../middleware/admin');
 
 const router = express.Router();
 
+// Get all questions of a quiz
+router.get('/', auth, admin, async (req, res) => {
+  const questions = await prisma.question.findMany({
+    where: {
+      quiz_id: parseInt(req.params.quiz_id),
+    },
+    orderBy: {
+      question_id: 'asc',
+    },
+  });
+  res.json(questions);
+});
 
+// Create a new question for a quiz
+router.post('/', auth, admin, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const quiz_id = parseInt(req.params.quiz_id);
+
+  const data = _.pick(req.body, ['content', 'answers']);
+  const question = await prisma.question.create({
+    data: {
+      content: data.content,
+      quiz_id: {
+        connect: {
+          quiz_id: quiz_id,
+        },
+      },
+      answers: {
+        create: data.answers,
+      },
+    },
+    include: {
+      answers: true,
+    },
+  });
+  res.status(201).json(question);
+});
+
+module.exports = router;
+
+// Get a question of a quiz
+router.get('/:question_id', auth, admin, async (req, res) => {
+  const question = await prisma.question.findUnique({
+    where: {
+      question_id: parseInt(req.params.question_id),
+    },
+    include: {
+      answers: true,
+    },
+  });
+  if (!question) return res.status(404).send('Question not found.');
+  res.json(question);
+});
+
+// Update a question of a quiz
+router.put('/:question_id', auth, admin, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const question_id = parseInt(req.params.question_id);
+
+  const data = _.pick(req.body, ['content', 'answers']);
+  const question = await prisma.question.update({
+    where: {
+      question_id: question_id,
+    },
+    data: {
+      content: data.content,
+      answers: {
+        deleteMany: {},
+        create: data.answers,
+      },
+    },
+    include: {
+      answers: true,
+    },
+  });
+  if (!question) return res.status(404).send('Question not found.');
+
+  res.json(question);
+});
+
+// Delete a question of a quiz
+router.delete('/:question_id', auth, admin, async (req, res) => {
+  const question = await prisma.question.delete({
+    where: {
+      question_id: parseInt(req.params.question_id),
+    },
+  });
+  if (!question) return res.status(404).send('Question not found.');
+  res.json(question);
+});
+
+// Get Questions of a Quiz without is_correct field of Answers
+router.get('/quiz', auth, async (req, res) => {
+  const questions = await prisma.question.findMany({
+    where: {
+      quiz_id: parseInt(req.params.quiz_id),
+    },
+    orderBy: {
+      question_id: 'asc',
+    },
+    select: {
+      question_id: true,
+      content: true,
+      answers: {
+        select: {
+          answer_id: true,
+          content: true,
+        },
+      },
+    },
+  });
+  res.json(questions);
+});
 
 module.exports = router;
