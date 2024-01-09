7726e26285c664d7230103e9ac9beb1865973cb4
Can Gokhan Kiziloglu <117171733+cankiziloglu@users.noreply.github.com>
2024-01-09 21:24:39 +0300

    heading font changed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { Heading, Text, Theme } from '@radix-ui/themes';
 function App() {
   return (
     <Theme>
-      <Heading size='5'>Hello world</Heading>
+      <Heading size='8'>Hello world</Heading>
       <Text as='p'>This is Can</Text>
     </Theme>
   );
