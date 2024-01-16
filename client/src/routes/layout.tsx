807df2ee9f6f6be3462e10ab6008ce4c3d36bdc1
Can Gokhan Kiziloglu <can.kiziloglu@gmail.com>
2024-01-16 14:43:49 +0300

    implemented theme and typography

--- client/src/routes/layout.tsx
+++ client/src/routes/layout.tsx
@@ -2,15 +2,18 @@
 import { Outlet } from 'react-router-dom';
 import NavBar from './navbar';
 import Footer from './footer';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const Layout = () => {
   return (
     <>
-      <NavBar />
-      <div id='main'>
-        <Outlet />
-      </div>
-      <Footer />
+      <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+        <NavBar />
+        <div id='main'>
+          <Outlet />
+        </div>
+        <Footer />
+      </ThemeProvider>
     </>
   );
 };
--- client/src/routes/navbar.tsx
+++ client/src/routes/navbar.tsx
@@ -1,5 +1,19 @@
+import { ModeToggle } from '@/components/mode-toggle';
+import { AvatarIcon } from '@radix-ui/react-icons';
+import { Link } from 'react-router-dom';
+
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <nav>
+      <h1 className='font-display'>Quiz-App</h1>
+      <div>
+        <Link to='/login'>
+          <AvatarIcon />
+        </Link>
+        <ModeToggle />
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
