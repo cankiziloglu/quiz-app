d629d7a16b139e067da2a845f3ca58f3cb84cd9d
Can Gokhan Kiziloglu <can.kiziloglu@gmail.com>
2024-01-18 20:15:20 +0300

    installed axios and react-query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,21 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import './index.css';
 import router from './routes/routes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
