5c31c9cef0d404c1c30261f48c5da4bcf9b9713a
Can Gokhan Kiziloglu <can.kiziloglu@gmail.com>
2024-02-06 13:18:48 +0300

    added authentication to private routes

--- client/src/routes/private-routes.tsx
+++ client/src/routes/private-routes.tsx
@@ -1,8 +1,15 @@
+import { AuthContext } from '@/context/auth-context';
+import { useContext } from 'react';
 import { Outlet } from 'react-router-dom';
+import Login from './login';
 
 const PrivateRoutes = () => {
   // TODO: Add authentication logic here
+  const auth = useContext(AuthContext);
 
+  if (!auth?.authState?.name) {
+    return <Login />;
+  }
   return <Outlet />;
 };
 
