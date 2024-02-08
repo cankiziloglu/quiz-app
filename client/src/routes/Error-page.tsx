6508bc2ceab12d14ff6216f195c79e6fb8fdcf83
Can Gokhan Kiziloglu <can.kiziloglu@gmail.com>
2024-02-08 19:57:33 +0300

    added navigation button to error page

--- client/src/routes/Error-page.tsx
+++ client/src/routes/Error-page.tsx
@@ -1,16 +1,29 @@
-import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
+import { HomeIcon } from '@radix-ui/react-icons';
+import {
+  isRouteErrorResponse,
+  useNavigate,
+  useRouteError,
+} from 'react-router-dom';
 
 export default function ErrorPage() {
   const error = useRouteError();
-
+  const navigate = useNavigate();
   return (
-    <div id='error-page'>
-      <h1>Oops!</h1>
-      <p>
+    <div
+      id='error-page'
+      className='flex flex-col justify-center items-center text-center p-8 h-dvh'
+    >
+      <h1 className='text-3xl font-display p-8'>Oops!</h1>
+      <h3 className='text-xl font-bold p-8'>
         {isRouteErrorResponse(error)
           ? 'Page Not Found'
           : 'Sorry, an unexpected error has occurred.'}
-      </p>
+      </h3>
+      <Button size='lg' onClick={() => navigate('/')}>
+        <HomeIcon className='mr-3 h-4 w-4' />
+        Go to Home
+      </Button>
     </div>
   );
 }
