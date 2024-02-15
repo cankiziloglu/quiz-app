62965834b855d0ab3e89348964bca9f31306b03b
Can Gokhan Kiziloglu <can.kiziloglu@gmail.com>
2024-02-15 13:42:56 +0300

    Refactor layout in quiz component

--- client/src/routes/quiz.tsx
+++ client/src/routes/quiz.tsx
@@ -3,13 +3,16 @@ import Questions from '@/components/questions';
 
 const Quiz = () => {
   return (
-    <div className='flex flex-col md:flex-row flex-grow overflow-auto gap-6 justify-center place-items-center items-center max-w-screen-lg w-full'>
-      <div id='question' className='w-full md:w-1/2'>
-        <Questions />
+    <div className='flex flex-col'>
+      <div className='flex flex-col md:flex-row flex-grow overflow-auto gap-6 justify-center place-items-center items-center max-w-screen-lg w-full'>
+        <div id='question' className='w-full md:w-1/2'>
+          <Questions />
+        </div>
+        <div id='answers' className='w-full md:w-1/2'>
+          <Answers />
+        </div>
       </div>
-      <div id='answers' className='w-full md:w-1/2'>
-        <Answers />
-      </div>
+      
     </div>
   );
 };
